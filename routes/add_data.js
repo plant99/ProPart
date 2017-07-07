ca84c926dfd53afe689371b63394a36fe7fe3a04
shivPadhi <shivashispadhi@gmail.com>
2017-07-07 17:17:37 +0530

    Add route to handle request for invites

--- routes/add_data.js
+++ routes/add_data.js
@@ -31,4 +31,79 @@ router.post('/invite', function(req, res, next){
 		}
 	})
 })
+
+router.post('/request_for_invite',function(req, res, next){
+	var id = req.body.id ;
+	var user = req.decoded._doc ;
+	if(id){
+		Invite.findOne({_id: id}, function(err, inviteFound){
+			if(inviteFound.author.name === user.username){
+				res.json({success:false, message: 'You only created the invite.'})
+			}else{
+				Invite.findOne({_id: id}, function(err, inviteFound){
+				if(err){
+					res.json({success: false, message: 'Invite not found'})
+				}else{
+					if(findIndexOf({username: user.username, id: user._id}, inviteFound.applicants) === -1){
+						inviteFound.applicants.push({username:user.username, id:user._id}) ;
+						User.findOne({username: user.username},function(err, user){
+							user.applied_invitations.push({id: id}) ;
+							user.save(function(err, savedUser){							if(!err){
+									inviteFound.save(function(err, savedInvite){
+										res.json({success: true, invite: savedInvite})
+									})
+								}else{
+
+									res.json({success:false})
+								}
+							})
+						})
+					}else{
+						res.json({success:false, message:'Already Applied'}) ;
+					}
+				}
+				})
+
+			}
+		})
+	}else{
+		res.json({success:false})
+	}
+})
 module.exports = router ;
+
+/*
+Invite.findOne({_id: id}, function(err, inviteFound){
+			if(err){
+				res.json({success: false, message: 'Invite not found'})
+			}else{
+				if(findIndexOf({username: user.username, id: user._id}, inviteFound.applicants) === -1){
+					inviteFound.applicants.push({username:user.username, id:user._id}) ;
+					User.findOne({username: user.username},function(err, user){
+						user.applied_invitations.push({id: id}) ;
+						user.save(function(err, savedUser){							if(!err){
+								inviteFound.save(function(err, savedInvite){
+									res.json({success: true, invite: savedInvite})
+								})
+							}else{
+
+								res.json({success:false})
+							}
+						})
+					})
+				}else{
+					res.json({success:false, message:'Already Applied'}) ;
+				}
+			}
+		})
+
+*/
+
+function findIndexOf(object, array){
+	for(var i=0 ;i<array.length ;i++){
+		if((object.username === array[i].username) && (object.id === array[i].id)){
+			return i ;
+		}
+	}
+	return -1
+}
