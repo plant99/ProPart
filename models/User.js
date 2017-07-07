31598a535a14f4944a38dc7e1572301463a017f2
shivPadhi <shivashispadhi@gmail.com>
2017-07-07 17:11:25 +0530

    Add applied invitations to User model

--- models/User.js
+++ models/User.js
@@ -9,5 +9,6 @@ module.exports = mongoose.model('user', new Schema({
 	username: String ,
 	password: String ,
 	active_invitations: Array,
-	image: String
+	image: String,
+	applied_invitations: Array
 }))
