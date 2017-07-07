bd5cbd6830eca15400b35d7985929804b0529a5a
shivPadhi <shivashispadhi@gmail.com>
2017-07-07 17:17:54 +0530

    Send user along with the invites

--- routes/get_data.js
+++ routes/get_data.js
@@ -2,13 +2,13 @@ var express = require('express') ;
 var router = express.Router() ;
 router.get('/', function(req, res, next){
 	Invite.find({}, function(err, invites){
-		res.json({invites:invites})
+		res.json({invites:invites,  user: req.decoded._doc})
 	})
 })
 router.get('/:category',function(req, res, next){
 	Invite.find({category: req.params.category}, function(err, invites){
 		if(invites.length){
-			res.json({success:true, invites:invites})
+			res.json({success:true, invites:invites, user: req.decoded._doc})
 		}else{
 			res.json({success: false})
 		}
